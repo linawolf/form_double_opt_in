a684407b9613728f75fcb6f81472e085019f5998
Daniel Kestler <daniel.kestler@medienreaktor.de>
2018-05-23 11:49:21 +0200

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Form Double Opt-In',
+    'description' => 'Double Opt-In for the TYPO3 CMS Form Framework',
+    'category' => 'plugin',
+    'author' => 'Daniel Kestler',
+    'author_email' => 'daniel.kestler@medienreaktor',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
