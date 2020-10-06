52559598e4f5c42dc6effb6b8d71f81d713a3353
Tanel Põld <tanel@brightside.ee>
2020-10-06 10:26:46 +0300

    Add opt-in completed notification mail feature

--- Classes/Controller/DoubleOptInController.php
+++ Classes/Controller/DoubleOptInController.php
@@ -1,6 +1,10 @@
 <?php
 namespace Medienreaktor\FormDoubleOptIn\Controller;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+
 /*
  * DoubleOptInController
  */
@@ -39,22 +43,70 @@ class DoubleOptInController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
     public function validationAction()
     {
         $success = FALSE;
-
+        $validated = FALSE;
         if ($this->request->hasArgument('hash')) {
             $hash = $this->request->getArgument('hash');
             $optIn = $this->optInRepository->findOneByValidationHash($hash);
 
             if ($optIn) {
-                $optIn->setIsValidated(TRUE);
-                $optIn->setValidationDate(new \DateTime);
-                $this->optInRepository->update($optIn);
+                $isValidated = $optIn->getIsValidated();
 
-                $this->signalSlotDispatcher->dispatch(__CLASS__, 'afterOptInValidation', [$optIn]);
+            // Simple notification e-mail
+                $this->configurationManager = $this->objectManager->get(ConfigurationManager::class);
+                $configuration = $this->configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+                $notificationMailEnable = $configuration['plugin.']['tx_formdoubleoptin_doubleoptin.']['persistence.']['notificationMailEnable'];
+                $notificationMailReceiverMail = $configuration['plugin.']['tx_formdoubleoptin_doubleoptin.']['persistence.']['notificationMailReceiverMail'];
 
-                $success = TRUE;
+                if (!$isValidated && $notificationMailEnable && $notificationMailReceiverMail) {
+                    $notificationMailReceiverName = $configuration['plugin.']['tx_formdoubleoptin_doubleoptin.']['persistence.']['notificationMailReceiverName'];
+                    $notificationMailSenderMail = $configuration['plugin.']['tx_formdoubleoptin_doubleoptin.']['persistence.']['notificationMailSenderMail'];
+                    $notificationMailSenderName = $configuration['plugin.']['tx_formdoubleoptin_doubleoptin.']['persistence.']['notificationMailSenderName'];
+                    $notificationMailSubject = $configuration['plugin.']['tx_formdoubleoptin_doubleoptin.']['persistence.']['notificationMailSubject'];
+
+                    $language = $optIn->getPagelanguage();
+                    $title = $optIn->getTitle();
+                    $givenName = $optIn->getGivenName();
+                    $familyName = $optIn->getFamilyName();
+                    $email = $optIn->getEmail();
+                    $company = $optIn->getCompany();
+                    $customerNumber = $optIn->getCustomerNumber();
+
+                    $mail = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Mail\MailMessage::class);
+                    if ($notificationMailSenderMail && $notificationMailSenderName) {
+                        $mail->from(new \Symfony\Component\Mime\Address($notificationMailSenderMail, $notificationMailSenderName));
+                    } elseif ($notificationMailSenderMail) {
+                        $mail->from(new \Symfony\Component\Mime\Address($notificationMailSenderMail));
+                    }
+                    if ($notificationMailReceiverMail && $notificationMailReceiverName) {
+                        $mail->to(new \Symfony\Component\Mime\Address($notificationMailReceiverMail, $notificationMailReceiverName));
+                    } elseif ($notificationMailReceiverMail) {
+                        $mail->to(new \Symfony\Component\Mime\Address($notificationMailReceiverMail));
+                    }
+                    if ($notificationMailSubject) {
+                        $mail->subject($notificationMailSubject);
+                    }
+                    $mail->text($language .', '. $title .', '. $givenName .', '. $familyName .', '.$email.', '.$company.', '.$customerNumber);
+                    $mail->html($language .'<br />'. $title .'<br />'. $givenName .'<br />'. $familyName .'<br />'.$email.'<br />'.$company.'<br />'.$customerNumber);
+                    $mail->send();
+                }
+
+            // Set validated if not yet
+                if (!$isValidated) {
+                    $optIn->setIsValidated(TRUE);
+                    $optIn->setValidationDate(new \DateTime);
+                    $this->optInRepository->update($optIn);
+                    $this->signalSlotDispatcher->dispatch(__CLASS__, 'afterOptInValidation', [$optIn]);
+                    $success = TRUE;
+                }
+
+            // If already validated
+                if ($isValidated) {
+                    $validated = TRUE;
+                }
             }
         }
 
         $this->view->assign('success', $success);
+        $this->view->assign('validated', $validated);
     }
 }
