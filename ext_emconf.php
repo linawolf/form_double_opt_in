81f8a44d250267b17c51e14b7b39ce40e5f23ccd
Stefan Froemken <froemken@gmail.com>
2024-09-06 21:11:18 +0200

    Set version to 12.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,11 +5,11 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Double Opt-In for the TYPO3 CMS Form Framework',
     'category' => 'plugin',
     'state' => 'stable',
-    'version' => '11.0.0',
+    'version' => '12.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
-            'form' => '11.5.0-11.5.99',
+            'typo3' => '12.4.0-12.4.99',
+            'form' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
