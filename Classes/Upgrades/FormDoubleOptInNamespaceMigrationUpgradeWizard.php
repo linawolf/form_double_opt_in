19fdaace1ce0a4389e729abeffd56212a851c289
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-09-17 15:35:35 +0200

    [TASK] Change in Upgrade Wizard registration and Configuration

--- Classes/Upgrades/FormDoubleOptInNamespaceMigrationUpgradeWizard.php
+++ Classes/Upgrades/FormDoubleOptInNamespaceMigrationUpgradeWizard.php
@@ -2,19 +2,17 @@
 
 declare(strict_types=1);
 
-namespace LinaWolf\FormDoubleOptIn\Updates;
+namespace LinaWolf\FormDoubleOptIn\Upgrades;
 
 use TYPO3\CMS\Install\Updates\ConfirmableInterface;
 use TYPO3\CMS\Install\Updates\Confirmation;
 use TYPO3\CMS\Install\Updates\DatabaseUpdatedPrerequisite;
 use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
 
-class FormDoubleOptInNamespaceMigration implements UpgradeWizardInterface, ConfirmableInterface
+#[UpgradeWizard('formDoubleOptInNamespaceMigrationMigration')]
+class FormDoubleOptInNamespaceMigrationUpgradeWizard implements UpgradeWizardInterface, ConfirmableInterface
 {
-    /**
-     * @var Confirmation
-     */
-    protected $confirmation;
+    protected Confirmation $confirmation;
 
     public function __construct()
     {
@@ -28,20 +26,14 @@ class FormDoubleOptInNamespaceMigration implements UpgradeWizardInterface, Confi
         );
     }
 
-    /**
-     * @return string Unique identifier of this updater
-     */
     public function getIdentifier(): string
     {
-        return 'FormDoubleOptInNamespaceMigrationMigration';
+        return 'formDoubleOptInNamespaceMigrationMigration';
     }
 
-    /**
-     * @return string Title of this updater
-     */
     public function getTitle(): string
     {
-        return 'form_double_opt_in: Migrate Form Finishers to the new Namespace';
+        return 'EXT:form_double_opt_in: Migrate Form Finishers to the new Namespace';
     }
 
     /**
@@ -53,9 +45,6 @@ class FormDoubleOptInNamespaceMigration implements UpgradeWizardInterface, Confi
             'finishers to `Medienreaktor\FormDoubleOptIn\Domain\Finishers\DoubleOptInFormFinisher` manually.';
     }
 
-    /**
-     * @return bool Whether an update is required (TRUE) or not (FALSE)
-     */
     public function updateNecessary(): bool
     {
         return true;
@@ -73,8 +62,6 @@ class FormDoubleOptInNamespaceMigration implements UpgradeWizardInterface, Confi
 
     /**
      * This upgrade wizard has informational character only, it does not perform actions.
-     *
-     * @return bool Whether everything went smoothly or not
      */
     public function executeUpdate(): bool
     {
@@ -83,8 +70,6 @@ class FormDoubleOptInNamespaceMigration implements UpgradeWizardInterface, Confi
 
     /**
      * Return a confirmation message instance
-     *
-     * @return Confirmation
      */
     public function getConfirmation(): Confirmation
     {
