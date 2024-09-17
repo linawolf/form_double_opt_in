540eac70c19a4cf8023328bb2cc877200cb4a1d6
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-09-17 14:21:57 +0200

    [TASK] CleanUp and Code Formatting

--- ext_tables.php
+++ ext_tables.php
@@ -1,17 +1,16 @@
 <?php
+declare(strict_types=1);
 
 use TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask;
 
 defined('TYPO3') || die('Access denied.');
 
-call_user_func(
-    function ($extKey) {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']
-        [TableGarbageCollectionTask::class]['options']['tables']
-        ['tx_formdoubleoptin_domain_model_optin'] = [
-            'dateField' => 'tstamp',
-            'expirePeriod' => 14,
-        ];
-    },
-    'form_double_opt_in',
-);
+call_user_func(static function () {
+    # Configuring the table garbage collection task
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']
+    [TableGarbageCollectionTask::class]['options']['tables']
+    ['tx_formdoubleoptin_domain_model_optin'] = [
+        'dateField' => 'tstamp',
+        'expirePeriod' => 14,
+    ];
+});
