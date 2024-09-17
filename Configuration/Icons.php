a150ad936f5eed7338b53c598e674cbc9e5e811c
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-09-17 14:48:54 +0200

    [TASK] Moved Icon registration configuration to Configuration folder

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'plugin-doubleoptin' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:form_double_opt_in/Resources/Public/Icons/PluginDoubleOptIn.svg',
+    ],
+];
